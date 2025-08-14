f577cd59f1b919d3133be6ffa4a4cc5fa3218e5d
Flix <flixgit@outlook.com>
2025-08-14 13:59:09 -0400

    made code SERIOUS :I

--- tesoar.js
+++ tesoar.js
@@ -1,24 +1,22 @@
-window.new = {
+<script>
+window._new = {
   create: {
     div: function(divName) {
       const div = document.createElement("div");
       div.id = divName;
       document.body.appendChild(div);
 
-      // Create a style    element usening  style
       const styleEl = document.createElement("style");
       document.head.appendChild(styleEl);
 
       return {
         h1: function(text) {
-          // this guy is for creating h1 elemecnts
           const h1 = document.createElement("h1");
           h1.textContent = text;
           div.appendChild(h1);
           return this;
         },
         p: function(text) {
-          // this cutie below is for the pea tags. (p tags sry for ma bard spellang)
           const p = document.createElement("p");
           p.textContent = text;
           div.appendChild(p);
@@ -34,12 +32,6 @@ window.new = {
             }
             css += "} ";
           }
-          // += css;
-          // :o;
-          // crasy;
-          // *crazay;
-          // **crazy;
-          // there we go;
           styleEl.textContent += css;
           return this;
         },
@@ -48,3 +40,4 @@ window.new = {
     }
   }
 };
+</script>
